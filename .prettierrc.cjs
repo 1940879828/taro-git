e5f89cfb9f174fbc6c98a6a86f90f333992906dd
YeJieChen <1940879828@qq.com>
2025-09-16 15:44:41 +0800

    添加侧边栏嵌入vue的webview

---
+++ .prettierrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  // 每行代码最大长度，超过则换行，默认 80
+  printWidth: 120,
+  // 每个缩进级别的空格数，默认 2
+  tabWidth: 2,
+  // 使用制表符（tab）而不是空格进行缩进，默认 false
+  useTabs: false,
+  // 在语句末尾添加分号，默认 true
+  semi: false,
+  // 使用单引号而不是双引号，默认 false
+  singleQuote: true,
+  // 在 JSX 中使用单引号而不是双引号，默认 false
+  jsxSingleQuote: false,
+  // 在对象属性和数组元素的末尾添加逗号，"es5" 表示遵循 ES5 规范添加逗号
+  trailingComma: 'es5',
+  // 在对象字面量的花括号内添加空格，默认 true
+  bracketSpacing: true,
+  // 将多行 JSX 元素的尖括号放在最后一行的末尾，而不是单独一行，默认 false
+  bracketSameLine: false,
+  // 在箭头函数参数只有一个时，省略括号，默认 "always" 表示总是添加括号
+  arrowParens: 'always',
+  // 文件结束时插入一个换行符，默认 "always"
+  endOfLine: 'lf',
+}
