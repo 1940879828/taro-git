ae5c8c5f2b686bc4f82ece231b38a7fb67364fb3
YeJieChen <1940879828@qq.com>
2025-09-19 11:10:38 +0800

    完成配置多文件页面vue构建

--- packages/view-vue/vite.config.ts
+++ packages/view-vue/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        panel: resolve(__dirname, 'src/panel/index.html'),
+        sidebar: resolve(__dirname, 'src/sidebar/index.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+    outDir: '../extension/out/view-vue',
+  },
 })
