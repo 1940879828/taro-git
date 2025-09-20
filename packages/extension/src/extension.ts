bade120e6b5f4885c912af182c73e0a1cc82472d
yejiechen <1940879828@qq.com>
2025-09-20 10:12:24 +0800

    插入panel区域webview

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -4,11 +4,18 @@ import { ViewProviderSidebar } from './view-provider/view-provider-sidebar'
 
 export function activate(context: ExtensionContext) {
   const handles = getHandlers()
+
   const viewProvidersSidebar = new ViewProviderSidebar(context, handles)
   const sidebarViewDisposable = window.registerWebviewViewProvider('sidebar-view-container', viewProvidersSidebar, {
     webviewOptions: { retainContextWhenHidden: true },
   })
   context.subscriptions.push(sidebarViewDisposable)
+
+  const viewProvidersPanel = new ViewProviderSidebar(context, handles)
+  const panelViewDisposable = window.registerWebviewViewProvider('panel-view-container', viewProvidersPanel, {
+    webviewOptions: { retainContextWhenHidden: true },
+  })
+  context.subscriptions.push(panelViewDisposable)
 }
 
 export function deactivate() {}
