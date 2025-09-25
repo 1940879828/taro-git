f9f9b53c77527e22a20cb67e88d16e81dc77b949
YeJieChen <1940879828@qq.com>
2025-09-25 18:34:53 +0800

    feat: 侧边栏头部静态UI未完成暂存

--- packages/view-vue/src/components/sidebar/SidebarHeader/SidebarHeader.vue
+++ packages/view-vue/src/components/sidebar/SidebarHeader/SidebarHeader.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>CommitList</div>
+  <div class="sidebar-header">
+    <div class="tabs">
+      <div v-for="(item,index) in ['提交','搁置']" :class="`tab ${currentTab === index ? 'active' : ''}`" @click="currentTab = index" :key="index">
+        {{item}}
+      </div>
+    </div>
+    <div class="settings">
+        
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
 
-<style scoped></style>
+const currentTab = ref(0)
+</script>
+
+<style scoped>
+.sidebar-header {
+  border: 1px solid #333;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 4px;
+  background: #3B4754;
+}
+
+.tabs {
+  display: flex;
+}
+
+.tab {
+  padding: 4px 8px;
+  cursor: pointer;
+}
+
+.tab.active {
+  border-bottom: 2px solid #4a88c7;
+  font-weight: bold;
+}
+</style>
